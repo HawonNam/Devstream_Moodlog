8833d12be150c3d9a102cd19543d9e54c1bfd322
HawonNam <ktion1220@hufs.ac.kr>
2025-04-09 12:35:36 +0900

    init: 프로젝트 초기코드 업로드

---
+++ src/main/java/com/example/Moodlog/MoodlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.Moodlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Moodlog/MoodlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Moodlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
